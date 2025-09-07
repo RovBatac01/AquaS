e67b975286229c61c7d28c50ac2ed3de287dec96
Rov <rovicbatacandolo@gmail.com>
2025-09-07 15:51:29 +0800

    try

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 // Import necessary modules
 const express = require("express");
 const mysql = require('mysql2/promise');
-const { SerialPort } = require("serialport");
-const { ReadlineParser } = require("@serialport/parser-readline");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const http = require("http");
